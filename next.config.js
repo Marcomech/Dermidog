98671e240fc42c93f683e4731e4955f70d95d240
Marco Mecha <mmecha2001@gmail.com>
2023-05-29 12:27:32 -0300

    example

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    adjustFontFallbacks: true,
+    adjustFontFallbacksWithSizeAdjust: true,
+    allowedRevalidateHeaderKeys: [],
+    amp: {
+      experimental: {
+        optimizeFonts: true,
+      },
+    },
+  }
 }
 
 module.exports = nextConfig
--- public/Diagnostico/index.tsx
+++ public/Diagnostico/index.tsx
@@ -2,10 +2,14 @@ import { Prurito } from "./PRURITO";
 import { Seborrea } from "./SEBORREA";
 import { Simetria } from "./SIMETRIA";
 import { Area } from "./AREA";
+import { SignoGral } from "./SIGNO_GENERAL";
+import { Signo } from "./SIGNO";
 
 export {
   Prurito,
   Seborrea,
   Simetria,
   Area,
+  SignoGral,
+  Signo
 }
