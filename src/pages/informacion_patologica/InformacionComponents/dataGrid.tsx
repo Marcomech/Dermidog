d14d65ee2737e8aabe4a3e0d6bb6e375cd23cce0
Marco Mecha <mmecha2001@gmail.com>
2023-05-31 10:57:16 -0300

    dialog

--- src/pages/informacion_patologica/InformacionComponents/dataGrid.tsx
+++ src/pages/informacion_patologica/InformacionComponents/dataGrid.tsx
@@ -2,37 +2,58 @@ import React from 'react';
 import Grid from "@mui/material/Unstable_Grid2"
 import { FullDataBase } from '../../../../public/data/index'
 import GridItem from './gridItem';
+import { List } from '@mui/material';
 
 interface DataGridProps {
     capitulos: FullCharapters
 }
 
 export default function DataGrid({ capitulos }: DataGridProps) {
+
+    if (!capitulos) {
+        return <div>No hay enfermedades disponibles para este capitulo</div>
+    }
+
     return (
-        <Grid
-            container
-            spacing={2}>
+        <List>
             {
-                (capitulos) ?
-                    capitulos.ENFERMEDADES.map((enfermedadCap: string) => (
-                        FullDataBase.map((enfermedadData: FullData, index: number) => {
-                            const string_1 = enfermedadData.NAME.normalize('NFD').replace(/\p{Diacritic}/gu, '').toUpperCase();
-                            const string_2 = enfermedadCap.normalize('NFKD').replace(/\p{Diacritic}/gu, '').toUpperCase();
-                            if (string_1 === string_2) {
-                                return (
-                                    <Grid xs={12} sm={6} md={4} lg={3} key={index} >
-                                        <GridItem
-                                            id={enfermedadData.ID}
-                                            data={enfermedadData.DATA}
-                                            name={enfermedadData.NAME} />
-                                    </Grid>
-                                );
-                            }
-                            return null
-                        })
-                    ))
-                    : null
+                capitulos.ENFERMEDADES.map((enfermedadCap: string) => (
+                    FullDataBase.map((enfermedadData: FullData, index: number) => {
+                        //const string_1 = enfermedadData.NAME.normalize('NFD').replace(/\p{Diacritic}/gu, '').toUpperCase();
+                        //const string_2 = enfermedadCap.normalize('NFKD').replace(/\p{Diacritic}/gu, '').toUpperCase();
+                        //if (string_1 === string_2) {
+                        //    return (
+                        //        <Grid xs={12} sm={6} md={4} lg={3} key={index} >
+                        //            <GridItem
+                        //                id={enfermedadData.ID}
+                        //                data={enfermedadData.DATA}
+                        //                name={enfermedadData.NAME} />
+                        //        </Grid>
+                        //    );
+                        //}
+                        return (compareNames(
+                            enfermedadCap,
+                            enfermedadData,
+                            index
+                        ));
+                    })
+                ))
             }
-        </Grid>
+        </List>
     )
+}
+function compareNames(enfermedadCap: string, enfermedadData: FullData, index: number) {
+    const string_1 = enfermedadData.NAME.normalize('NFD').replace(/\p{Diacritic}/gu, '').toUpperCase();
+    const string_2 = enfermedadCap.normalize('NFKD').replace(/\p{Diacritic}/gu, '').toUpperCase();
+    if (string_1 === string_2) {
+        return (
+            <Grid xs={12} sm={6} md={4} lg={3} key={index} >
+                <GridItem
+                    id={enfermedadData.ID}
+                    data={enfermedadData.DATA}
+                    name={enfermedadData.NAME} />
+            </Grid>
+        );
+    }
+    return null
 }
